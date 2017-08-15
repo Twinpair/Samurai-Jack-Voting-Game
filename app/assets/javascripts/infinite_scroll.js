38126c681cb9e481198a7181945639fda22fedfa
Eric <twinpaireric@gmail.com>
2017-08-14 17:23:52 -0700

    Modify infinite scroll height value

--- app/assets/javascripts/infinite_scroll.js
+++ app/assets/javascripts/infinite_scroll.js
@@ -3,7 +3,7 @@ $(document).on('ready page:load', function () {
   if ($('#infinite-scrolling').size() > 0) {
     $(window).on('scroll', function() {
       var more_cards_url = $('.pagination a.next_page').attr('href');
-      if (!isLoading && more_cards_url && $(window).scrollTop() > $(document).height() - $(window).height() - 80) {
+      if (!isLoading && more_cards_url && $(window).scrollTop() > $(document).height() - $(window).height() - 100) {
         isLoading = true;
         $.getScript(more_cards_url).done(function (data,textStatus,jqxhr) {
           isLoading = false;
